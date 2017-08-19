1b1d2b58f921ce0833d691b073178b0e59520367
Mikael <mikael.popowicz@gmail.com>
2017-08-19 16:24:01 +0200

    Add .gitignore. Add eslint to sanitize code and make it more readable

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console": 0,
+        "no-undef": 0,
+        "no-inner-declarations": 0,
+        "no-unused-vars": 0,
+        "no-useless-escape": 0,
+        "no-empty": 0,
+        "no-control-regex": 0,
+        "no-redeclare": 0,
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};

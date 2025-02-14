637a45bd9bfea6f3a90be8851387fbd7cc90c8ef
hhurz <hhurz@online.de>
2025-02-14 21:07:38 +0100

    Release 1.31

--- tableExport.js
+++ tableExport.js
@@ -1,9 +1,9 @@
 /**
  * @preserve tableExport.jquery.plugin
  *
- * Version 1.30.0
+ * Version 1.31.0
  *
- * Copyright (c) 2015-2024 hhurz,
+ * Copyright (c) 2015-2025 hhurz,
  *   https://github.com/hhurz/tableExport.jquery.plugin
  *
  * Based on https://github.com/kayalshri/tableExport.jquery.plugin
